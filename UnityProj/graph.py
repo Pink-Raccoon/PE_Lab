682fefd03c7f0fdba39556323cc007c331e05417
Michel Steiner <michel.steiner@gmail.com>
2023-04-22 23:12:56 +0200

    code ergänzung

--- UnityProj/graph.py
+++ UnityProj/graph.py
@@ -1,36 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../UnityProj/TimeSeries/time_seriesElastic.csv")
+df = pd.read_csv("../UnityProj/time_seriesElastic.csv")
 
 plt.figure(figsize=(20,20))
 plt.subplot(4,1,1)
 plt.plot(df["currentTimeStep"], df[" cubeRomeo.position.x"])
-plt.ylabel("v")
-plt.xlabel("t")
+plt.ylabel("[s] = m")
+plt.xlabel("[t] = s")
 plt.title("Ort als Funktion der Zeit")
-
+plt.show()
 
 
 plt.subplot(4,1,2)
 plt.plot(df["currentTimeStep"], df[" cubeRomeo.velocity.x"])
-plt.ylabel("v")
-plt.xlabel("t")
+plt.ylabel("[v] = m/s")
+plt.xlabel("[t] = s")
 plt.title("Geschwindigkeit als Funktion der Zeit")
-
+plt.show()
 
 
 plt.subplot(4,1,3)
 plt.plot(df["currentTimeStep"], df[" cubeRomeoKinetic"])
-plt.ylabel("J")
-plt.xlabel("t")
+plt.ylabel("[J] = Nm")
+plt.xlabel("[t] = s")
 plt.title("Kinetische Energie als Funktion der Zeit")
-
+plt.show()
 
 plt.subplot(4,1,4)
 plt.plot(df["currentTimeStep"], df[" springPotentialEnergy"])
-plt.ylabel("J")
-plt.xlabel("t")
+plt.ylabel("[J] = Nm")
+plt.xlabel("[t] = s")
 plt.title("Potentielle Energie als Funktion der Zeit")
 
 plt.show()
