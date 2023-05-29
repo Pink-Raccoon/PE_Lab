3382749150f0ce9dc7daa3e145f7f4b4e0b24227
vukim <vukim@students.zhaw.ch>
2023-05-29 09:08:19 +0200

    added and changed plots

--- UnityProj/graphElastic.py
+++ UnityProj/graphElastic.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../UnityProj/time_seriesElastic.csv")
+df = pd.read_csv("./TimeSeries/time_seriesElastic_pt2.csv")
 
-plt.figure(figsize=(20,20))
-plt.subplot(4,1,1)
+#plt.figure(figsize=(20,20))
+#plt.subplot(4,1,1)
 plt.plot(df["currentTimeStep"], df[" cubeRomeo.position.x"])
 plt.ylabel("[s] = m")
 plt.xlabel("[t] = s")
@@ -12,8 +12,7 @@ plt.title("Ort als Funktion der Zeit")
 plt.savefig('../Semesterprojekt Physik Engines/images/Elastisch/OrtAlsFunktionDerZeit.png', dpi=300, bbox_inches='tight')
 plt.show()
 
-
-plt.subplot(4,1,2)
+#plt.subplot(4,1,2)
 plt.plot(df["currentTimeStep"], df[" cubeRomeo.velocity.x"])
 plt.ylabel("[v] = m/s")
 plt.xlabel("[t] = s")
@@ -21,7 +20,7 @@ plt.title("Geschwindigkeit als Funktion der Zeit")
 plt.savefig('../Semesterprojekt Physik Engines/images/Elastisch/GeschwindigkeitAlsFunktionDerZeit.png', dpi=300, bbox_inches='tight')
 plt.show()
 
-
+#%%
 plt.subplot(4,1,3)
 plt.plot(df["currentTimeStep"], df[" cubeRomeoKinetic"])
 plt.ylabel("[J] = Nm")
