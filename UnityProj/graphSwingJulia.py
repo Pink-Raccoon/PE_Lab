c2db2d4d7a3bf9cebc362b6434ff8d20618c4b25
vukim <vukim@students.zhaw.ch>
2023-05-30 18:49:44 +0200

    merged the plot to one

--- UnityProj/graphSwingJulia.py
+++ UnityProj/graphSwingJulia.py
@@ -9,20 +9,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv("./TimeSeries/timeSeriesRopeJulia.csv")
+df_rom = pd.read_csv("./TimeSeries/timeSeriesRopeRomeo.csv")
+df_jul = pd.read_csv("./TimeSeries/timeSeriesRopeJulia.csv")
 
 #plt.figure(figsize=(20,20))
-plt.plot(df[" cubeJulia.position.x"], df[" cubeJulia.position.y"])
+plt.plot(df_rom[" cubeRomeo.position.x"], df_rom[" cubeRomeo.position.y"], label="Romeo")
+plt.plot(df_jul[" cubeJulia.position.x"], df_jul[" cubeJulia.position.y"], label="Julia")
 plt.xlabel("Position x [s] = m")
 plt.ylabel("Position y [s] = m")
-plt.title("Bewegung Würfel Julia")
+plt.title("Bewegung Würfel Romeo & Julia")
+plt.legend()
 plt.savefig('../Semesterprojekt Physik Engines/images/ropeJulia/Ortdiagramm.png', dpi=300, bbox_inches='tight')
 plt.show()
 
 
-plt.plot(df["currentTimeStep"],df[" degree"])
+plt.plot(df_rom["currentTimeStep"],df_rom["degree"], label="Romeo")
+plt.plot(df_jul["currentTimeStep"],df_jul[" degree"], label="Julia")
 plt.ylabel("[s] = alpha")
 plt.xlabel("[t] = s")
+plt.legend()
 plt.title("Auslenkung in Grad als Funktion der Zeit")
 plt.savefig('../Semesterprojekt Physik Engines/images/ropeJulia/AuslenkungDeg.png', dpi=300, bbox_inches='tight')
 plt.show()
